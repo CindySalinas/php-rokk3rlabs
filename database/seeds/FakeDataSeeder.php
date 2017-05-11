e0cc1ef746d5753dddff81b12f9a4b376eeb80ec
Cindy Salinas <cindysalinas15@gmail.com>
2017-05-11 14:22:08 -0500

    Migrations and fake data

---
+++ database/seeds/FakeDataSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FakeDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i=0; $i < 10; $i++) {
+            $user = App\User::create([
+            	'first_name' => $faker->firstName,
+            	'last_name' =>  $faker->lastName,
+            	'email' =>  $faker->unique()->freeEmail,
+            	'password' => '123456',
+            ]);
+        }
+
+        for ($i=0; $i < 20; $i++) {
+            $product = App\Product::create([
+            	'name' => ucfirst($faker->word),
+            	'description' => $faker->text,
+            	'price' =>  $faker->randomFloat(2, $min = 1, 25000),
+            	'stock' => $faker->numberBetween(1,50),
+            ]);
+        }
+    }
+}
