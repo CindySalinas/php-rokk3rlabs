db6171c7add56e890c0d0450e6d77a533b175190
Cindy Salinas <cindysalinas15@gmail.com>
2017-05-11 18:53:11 -0400

    added endpoints

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,12 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\User;
+use App\Shoppingcart;
 
 class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::paginate(20);
-        return $products;
+
+        $user = User::where('id', 1)->first();
+        if(!isset($user['id']))
+          return Array( 'success'=> false);
+
+        $shoppingcart = $user->shoppingcart;
+
+        if(!isset($shoppingcart)){
+            $products = Product::paginate(20);
+            return $products;
+        }
+        else {
+          $products = $shoppingcart->products;
+          $ids = Array();
+
+          foreach ($products as $key => $value) {
+            $ids[$key] = $value['id'];
+          }
+
+          $items =  Product::whereNotIn('id', $ids )->get();
+          return $items;
+        }
     }
 }
--- app/Http/Controllers/ShoppingcartsController.php
+++ app/Http/Controllers/ShoppingcartsController.php
@@ -4,15 +4,64 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\User;
+use App\Product;
+
 use App\Shoppingcart;
 
 class ShoppingcartsController extends Controller
 {
     public function index(Request $request)
     {
-        $user = Auth::user();
+        // $user = Auth::user();
+
+        $user = User::where('id', 1)->first();
+
+        if(!isset($user['id']))
+          return Array( 'success'=> false);
 
         $shoppingcart = $user->shoppingcart;
 
+        if(!isset($shoppingcart))
+          return Array();
+
+        $products = $shoppingcart->products;
+
+        return $products;
+    }
+
+    public function store(Request $request)
+    {
+      $id = $request->input('product_id');
+      $quantity = $request->input('quantity');
+
+      if(!isset($id) || !isset($quantity)){
+        return Array('success' => false);
+      }
+
+      $product = Product::where('id', $id)->first();
+
+      if($product['stock'] < $quantity){
+        return Array('success2' => false, 'message' => 'not enougth in existence');
+      }
+
+      $user = User::where('id', 1)->first();
+      $shoppingcart = $user->shoppingcart;
+
+      $newStock = $product['stock'] - $quantity;
+
+      if(!isset($shoppingcart)){
+        $shoppingcart = $user->shoppingcart()->create([]);
+        $shoppingcart->products()->attach($product);
+        $product['stock'] = $newStock;
+        $product->save();
+        return Array('success' => true);
+      }
+      else{
+        $shoppingcart->products()->attach($product);
+        $product['stock'] = $newStock;
+        $product->save();
+        return Array('success' => true);
+      }
     }
 }
